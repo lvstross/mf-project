902c2db22644095260118272f36d46ab811575a3
Levi Gonzales <lvstross@gmail.com>
2022-08-05 09:07:37 -0600

    bump s3 bucket version

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -4,7 +4,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
+// comment
 export default () => {
     return (
         <>
